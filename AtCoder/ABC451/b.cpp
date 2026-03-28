3cdb453fc99e4f6174ab266e62e8b6d3f86480d9
yutomi141 <yuto.mi1218816@gmail.com>
2026-03-28 21:45:41 +0900

    feat: solve ABC Problem B (Department headcount change)

---
+++ AtCoder/ABC451/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N,M;
+  cin >> N >> M;
+  
+  vector<int> C(M);
+  for (int i = 0; i < N; i++){
+    int A,B;
+    cin >> A >> B;
+    
+    C.at(A - 1)--;
+    C.at(B - 1)++;
+  }
+  
+  for (int j = 0; j < M; j++){
+    cout << C.at(j) << endl;
+  }
+}
