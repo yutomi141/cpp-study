4d2005f9b340435fabee74aef9786fe36d91ff7c
yutomi141 <yuto.mi1218816@gmail.com>
2026-04-15 09:37:29 +0900

    feat: Solve ABC306-A

---
+++ AtCoder/ABC306/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int N;
+  string S;
+  cin >> N >> S;
+  
+  for (int i = 0; i < N; i++){
+    cout << S.at(i) << S.at(i);
+  }
+  
+  cout << endl;
+}
